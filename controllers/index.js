d54a0c8fcb54abd0a69d51644f4ab293a8e5e996
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-25 14:05:19 +0100

    Categories taken from DB

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,14 @@ const genres = require('../config/routes/genres');
 const services = require('../services/index');
 
 // controllers
-const user_controller = require('./user');
-genres_constroller = require('./genres')
-
+const user_controller = require('./user'),
+    genres_constroller = require('./genres'),
+    cat_controller=require('./cat');
 // create a controllers object for map all the controllers
 const controllers = {
     user: user_controller(services),
-    genre: genres_constroller(services)
+    genre: genres_constroller(services),
+    cat: cat_controller(services)
 }
 
 // export our controllers object
