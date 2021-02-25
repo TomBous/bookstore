5ad1d306530b65c4f85221eb1386ab4fad07f769
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-25 17:19:38 +0100

    Livre added

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,19 @@ const genres = require('../config/routes/genres');
 const services = require('../services/index');
 
 // controllers
-const user_controller = require('./user'),
-    genres_constroller = require('./genres'),
-    cat_controller=require('./cat');
+const user_controller = require('./user');
+const genres_constroller = require('./genres');
+const cat_controller=require('./cat');
+const livre_controller=require('./livre');
+
+
+
 // create a controllers object for map all the controllers
 const controllers = {
     user: user_controller(services),
+    cat:cat_controller(services),
     genre: genres_constroller(services),
-    cat: cat_controller(services)
+    livre: livre_controller(services)
 }
 
 // export our controllers object
